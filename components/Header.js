1279533d8a8cd919bfbdc613da4eb2fd07a3b1b0
woojin.Kim <wjinkim21@naver.com>
2022-02-09 14:46:53 +0900

    수정 : Header 디자인

--- components/Header.js
+++ components/Header.js
@@ -5,15 +5,15 @@ export default function Header() {
         <header className='flex flex-row py-2 -ml-3 justify-between'>
             <div>
                 <Link href="/">
-                    <a href="#none" className="btn btn-link">Caffe</a>    
+                    <a className="btn btn-link">Caffe</a>    
                 </Link>      
             </div>
             <div>
                 <Link href="/introduce">
-                    <a href="/introduce" className="btn btn-link">Introduce</a>
+                    <a className="btn btn-link">Introduce</a>
                 </Link>
                 <Link href="/order">
-                    <a href="#none" className="btn btn-link">Order</a>
+                    <a className="btn btn-link">Order</a>
                 </Link>
                 <a href="#none" className="btn btn-link">Recruit</a> 
                 <a href="#none" className="btn btn-link">Contact us</a> 
